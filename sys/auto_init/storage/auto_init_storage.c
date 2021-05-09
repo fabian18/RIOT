f226b15e06c64e4e4e25fec997394246eecf044d
Fabian Hüßler <fabian.huessler@st.ovgu.de>
2021-05-09 16:13:10 +0200

    sys/auto_init: Allow auto-initialization of storage devices

---
+++ sys/auto_init/storage/auto_init_storage.c
@@ -0,0 +1,23 @@
+/*
+ * Copyright 2021 Otto-von-Guericke-Universität Magdeburg
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser General
+ * Public License v2.1. See the file LICENSE in the top level directory for more
+ * details.
+ */
+
+/**
+ * @ingroup sys_auto_init_storage
+ *
+ * @{
+ * @file
+ * @brief   Implementation of the auto initialization of storage devices
+ *
+ * @author  Fabian Hüßler <fabian.huessler@ovgu.de>
+ *
+ * @}
+ */
+
+void auto_init_storage(void)
+{
+}
