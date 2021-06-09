d8108b2666b096ec64d9fa4cedcb6d4ebdfc3417
Fabian Hüßler <fabian.huessler@st.ovgu.de>
2021-06-09 19:45:10 +0200

    boards/rpi-pico: add raspberry pi pico

---
+++ boards/rpi-pico/include/board.h
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) 2021 Otto-von-Guericke Universität Magdeburg
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup     boards_rpi-pico
+ * @{
+ *
+ * @file
+ * @brief       Board specific configuration for the Raspberry Pi Pico
+ *
+ * @author      Marian Buschsieweke <marian.buschsieweke@ovgu.de>
+ * @author      Fabian Hüßler <fabian.huessler@ovgu.de>
+ */
+
+#ifndef BOARD_H
+#define BOARD_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void board_init(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* BOARD_H */
+/** @} */
+
---
+++ boards/rpi-pico/include/periph_conf.h
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) 2021 Otto-von-Guericke-Universität Magdeburg
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup         boards_rpi-pico
+ * @{
+ *
+ * @file
+ * @brief           Configuration of CPU peripherals for the Raspberry Pi Pico
+ *
+ * @author          Marian Buschsieweke <marian.buschsieweke@ovgu.de>
+ * @author          Fabian Hüßler <fabian.huessler@ovgu.de>
+ */
+
+#ifndef PERIPH_CONF_H
+#define PERIPH_CONF_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* PERIPH_CONF_H */
+/** @} */
