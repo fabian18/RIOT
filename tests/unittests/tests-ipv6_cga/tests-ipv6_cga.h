e5f7d6e4f1ef21ddfe14eccdec165158078437cc
Fabian Hüßler <fabian.huessler@st.ovgu.de>
2022-04-15 20:21:34 +0200

    tests/unittests: add unittest for Cryptographically Generated Addresses

---
+++ tests/unittests/tests-ipv6_cga/tests-ipv6_cga.h
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2022 Otto-von-Guericke-Universität Magdeburg
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @addtogroup  unittests
+ * @{
+ *
+ * @file
+ * @brief       Unittests for the ``ipv6_cga`` module
+ *
+ * @author      Fabian Hüßler <fabian.huessler@ovgu.de>
+ */
+
+#ifndef TESTS_IPV6_CGA_H
+#define TESTS_IPV6_CGA_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @brief   Entry point of the IPv6 CGA test suite
+ */
+void tests_ipv6_cga(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* TESTS_IPV6_CGA_H */
+/** @} */
