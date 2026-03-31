a667fb38b61785051d07dedeafe6f30e97925bee
Fabian Hüßler <fabian.huessler@ml-pa.com>
2026-03-31 13:33:46 +0200

    fixup! tests/drivers: add mtd_eeprom test

--- tests/drivers/mtd_eeprom/tests/01-run.py
+++ tests/drivers/mtd_eeprom/tests/01-run.py
@@ -1,10 +1,7 @@
 #!/usr/bin/env python3
 
-# Copyright (C) 2026 ML!PA Consulting GmbH
-#
-# This file is subject to the terms and conditions of the GNU Lesser
-# General Public License v2.1. See the file LICENSE in the top level
-# directory for more details.
+# SPDX-FileCopyrightText: 2026 ML!PA Consulting GmbH
+# SPDX-License-Identifier: LGPL-2.1-only
 
 import sys
 from testrunner import run_check_unittests
